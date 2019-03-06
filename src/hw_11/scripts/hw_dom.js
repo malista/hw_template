3b925ca19be9a23ad3f336ed50b5b7b979a58ae0
EY <yaroshynska@gmail.com>
2019-03-06 12:01:26 +0200

    Update homework 11
    Signed-off-by: EY <yaroshynska@gmail.com>

--- src/hw_11/scripts/hw_dom.js
+++ src/hw_11/scripts/hw_dom.js
@@ -4,26 +4,22 @@ function lightWidget(rootElement) {
     const button2 = rootElement.querySelector('.light_yellow');
     const button3 = rootElement.querySelector('.light_red');
 
-/*
-    function lightOff(e1, e2, e3) {
-        console.log(e1)
-        e1.onclick = () => toggle();
+    button1.onclick = () => {
+        enlight(button1);
+        dislight(button2);
+        dislight(button3);
+
+    };
+    button2.onclick = () => {
+        enlight(button2);
+        dislight(button1);
+        dislight(button3);
     }
-
-    function toggle(e1, e2, e3) {
-        dislight(e1);
-        dislight(e2);
-        dislight(e3);
-        enlight(e1);
+    button3.onclick = () => {
+        enlight(button3);
+        dislight(button2);
+        dislight(button1);
     }
-    */
-   button1.onclick= () => enlight(button1);
-   button2.onclick = () => enlight(button2);
-   button3.onclick = () => enlight(button3);
-
-  
-
-
 
     function enlight(el) {
         el.classList.add('active');
@@ -32,10 +28,6 @@ function lightWidget(rootElement) {
     function dislight(elem) {
         elem.classList.remove('active');
     }
-
-
-    console.log('b1')
-    console.log(button1)
-
 }
+
 export { lightWidget };
